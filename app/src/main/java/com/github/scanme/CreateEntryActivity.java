33a38acf6c62966e2a6b981030891bd4d506c2cb
Alexis Siguenza <andoremmc2@gmail.com>
2019-03-08 19:44:49 -0800

    Taken images now displayed on Create Entry screen
    - Image is stored on device's external app storage
    - Generates a unique ID for each image taken

--- app/src/main/java/com/github/scanme/CreateEntryActivity.java
+++ app/src/main/java/com/github/scanme/CreateEntryActivity.java
@@ -3,14 +3,23 @@ package com.github.scanme;
 import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Environment;
 import android.provider.MediaStore;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.RelativeLayout;
+
+import java.io.File;
+import java.util.UUID;
 
 public class CreateEntryActivity extends AppCompatActivity {
 
@@ -18,6 +27,8 @@ public class CreateEntryActivity extends AppCompatActivity {
 
     private RelativeLayout cameraBackground;
     private FloatingActionButton cameraButton;
+    private File imageFile;
+    private ImageView entryImage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +37,7 @@ public class CreateEntryActivity extends AppCompatActivity {
 
         cameraBackground = findViewById(R.id.cameraBackground);
         cameraButton = findViewById(R.id.cameraButton);
+        entryImage = findViewById(R.id.entryImage);
     }
 
     public void takePicture(View view) {
@@ -37,6 +49,7 @@ public class CreateEntryActivity extends AppCompatActivity {
             // Permission already been granted
             final int REQUEST_IMAGE_CAPTURE = 1;
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Call System Camera App
+            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, createImageURI());
             if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
@@ -55,6 +68,22 @@ public class CreateEntryActivity extends AppCompatActivity {
                 return;
             }
         }
+    }
+
+    @Override
+    protected void onActivityResult(int request, int result, Intent intentData) {
+        if (request == PERMREQ_CAMERA && result == RESULT_OK) {
+            Bitmap resImage = BitmapFactory.decodeFile(imageFile.getAbsolutePath());
+            entryImage.setImageBitmap(resImage);
+            entryImage.setVisibility(View.VISIBLE);
+            cameraButton.setVisibility(View.GONE);
+        }
+    }
+
+    public Uri createImageURI() {
+        imageFile = new File(getExternalFilesDir(Environment.DIRECTORY_PICTURES), "IMG_" + UUID.randomUUID().toString() + ".png");
+        Uri fileProvider = FileProvider.getUriForFile(CreateEntryActivity.this, "com.codepath.fileprovider", imageFile);
+        return fileProvider;
     }
 
 }
