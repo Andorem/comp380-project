1d4140c1ac65262879f5316ef42dd67571255f8b
Alexis Siguenza <andoremmc2@gmail.com>
2019-03-10 11:29:54 -0700

    Added save button view in Create Entry screen

--- app/src/main/java/com/github/scanme/CreateEntryActivity.java
+++ app/src/main/java/com/github/scanme/CreateEntryActivity.java
@@ -15,6 +15,7 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 
@@ -54,6 +55,10 @@ public class CreateEntryActivity extends AppCompatActivity {
                 startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
             }
         }
+    }
+
+    public void saveEntry(View view) {
+
     }
 
     @Override
