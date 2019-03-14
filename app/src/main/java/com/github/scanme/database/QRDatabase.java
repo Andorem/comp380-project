395511550acda2e063582af7871a0cf8fcb393fd
Alexis Siguenza <andoremmc2@gmail.com>
2019-03-14 14:30:40 -0700

    Fix for database compile errors
    - Disabled database export schema
    - Ensured primary key for QR is non-null
    - Resolved database constructor conflicts

--- app/src/main/java/com/github/scanme/database/QRDatabase.java
+++ app/src/main/java/com/github/scanme/database/QRDatabase.java
@@ -9,13 +9,13 @@ import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
 
-@Database(entities = {QR.class}, version = 1)
+@Database(entities = {QR.class}, version = 1, exportSchema = false)
 abstract class QRDatabase extends RoomDatabase {
 
     // Dao getter for issuing queries
     public abstract QRDao qrDao();
 
-    /* This eensures the database is a singleton:
+    /* This ensures the database is a singleton:
           Instantiating a Room database takes a lot of resources.
           This makes sure there is only one instance of the database at a time.
      */
