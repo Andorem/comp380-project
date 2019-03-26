8248309c3abc1e09204243724d110a2c27dbccb3
atak37 <hkratakii@gmail.com>
2019-03-26 15:07:33 -0700

    view entry activity

--- app/src/main/java/com/github/scanme/ViewEntry.java
+++ app/src/main/java/com/github/scanme/ViewEntry.java
@@ -1,28 +1,8 @@
 package com.github.scanme;
 
 import android.os.Bundle;
-import android.Manifest;
-import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Environment;
-import android.provider.MediaStore;
+
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.core.content.FileProvider;
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
-import com.github.scanme.database.QR;
-import com.github.scanme.database.QRRepository;
-import java.io.File;
-import java.util.UUID;
 import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
