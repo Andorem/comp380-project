c930d1865e5b3238fc2060e57cedee172f3996ff
Sherry886 <yu.si.876@my.csun.edu>
2019-04-09 15:48:28 -0700

    QRscan 4th try

--- app/src/main/java/com/github/scanme/QRScan.java
+++ app/src/main/java/com/github/scanme/QRScan.java
@@ -4,36 +4,49 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.provider.MediaStore;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.yzq.zxinglibrary.android.CaptureActivity;
+import com.yzq.zxinglibrary.common.Constant;
 
 import static com.github.scanme.CreateEntryActivity.PERMREQ_CAMERA;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 //click the scan button, take it into the system camera
 //scan the QR
 //get info--link to the entry, get the info
-public class QRScan extends AppCompatActivity {
+public class QRScan extends AppCompatActivity implements View.OnClickListener{
+    private View view;
+    private Button scanButton;
+    private TextView resultTv;
+    public QRScan(){ }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_qrscan);
+        scanButton = view.findViewById(R.id.scanButton);
+        resultTv = view.findViewById(R.id.resultTv);
+        scanButton.setOnClickListener(this);
     }
-    public void takePicture(View view) {
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            // No permission granted
-            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, PERMREQ_CAMERA);
-        }
-        else {
-            // Permission already been granted
-            final int REQUEST_IMAGE_CAPTURE = 1;
-            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE); // Call System Camera App
-            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, createImageURI());
-            if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+    @Override
+    public void onClick(View v){
+        Intent intent = new Intent(this, CaptureActivity.class);
+        this.startActivityForResult(intent,1111);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode,resultCode,data);
+        if(requestCode == 1111){
+            if(data != null){
+                String content = data.getStringExtra(Constant.CODED_CONTENT);
+                resultTv.setText("result of scan:" + content);
             }
         }
     }
