e1716a54e16ccf5350d04c2e251cc1d34a7c903f
atak37 <hkratakii@gmail.com>
2019-04-17 18:24:01 -0700

    set & get qr.getTitle & qr.getDesc added textview import widget as well

--- app/src/main/java/com/github/scanme/ViewEntryActivity.java
+++ app/src/main/java/com/github/scanme/ViewEntryActivity.java
@@ -11,14 +11,23 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class ViewEntryActivity extends AppCompatActivity {
 
     String ID;
     QR qr;
     ImageView entryPicture;
+    TextView titleOutput;
+    TextView descriptionOutput;
     QRRepository qrRepo = new QRRepository(getApplication());
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +41,13 @@ public class ViewEntryActivity extends AppCompatActivity {
         qr = getIntent().getParcelableExtra("QR");
         ID = qr.getId();
         entryPicture = findViewById(R.id.entryPicture);
+
+        //setter
+        titleOutput.setText(qr.getTitle());
+        titleOutput = findViewById(R.id.titleView);
+
+        descriptionOutput.setText(qr.getDescription());
+        descriptionOutput = findViewById(R.id.descriptionView);
 
 
         Log.d("VIEW_ENTRY", "ID retrieved: " + ID);
@@ -47,11 +63,13 @@ public class ViewEntryActivity extends AppCompatActivity {
             }
         });
     }
+    // end of oncreate method
 
 
-
-    protected void getImage( String filePath){
+    protected void getImage(String filePath){
         entryPicture.setImageBitmap(BitmapHandler.rotateImage(this, filePath));
     }
+
+
 
 }
